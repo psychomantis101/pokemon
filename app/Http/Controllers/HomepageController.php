54e479d3cd3c07e1807ccaee929daee8b91ab08f
Lee Carbutt <lee.carbutt@digitalbalance.co.uk>
2023-02-07 19:57:26 +0000

    catch em all!

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,23 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Api\GetPokemon;
-use App\Models\Pokemon;
-
 class HomepageController extends Controller
 {
     public function index()
     {
-//        $pokemon = Pokemon::all();
-//
-//        if ($pokemon->isEmpty()) {
-//            $getPokemon = new GetPokemon();
-//
-//            Pokemon::insert($getPokemon->getGenerationOne());
-//
-//            $pokemon = Pokemon::all();
-//        }
-
         return view('pokemon');
     }
 }
